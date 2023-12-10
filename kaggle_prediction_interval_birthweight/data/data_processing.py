964ede39f315873f0735c10f59849b70642160bc
Hunter R. Merrill <hunter.merrill@hunters-air.lan>
2023-12-10 09:02:58 -0600

    saving work

--- kaggle_prediction_interval_birthweight/data/data_processing.py
+++ kaggle_prediction_interval_birthweight/data/data_processing.py
@@ -28,8 +28,8 @@ class DataProcessor:
             One of "linear regression", "xgboost", or "neural network". Determines how data are
             imputed and standardized before modeling.
         standardization_parameters: Dict
-            Precomputed columnwise means and the decorrelation matrix, if already computed. None
-            during training, but at test time these values should be passed in.
+            Precomputed columnwise means and sds or decorrelation matrix, if already computed.
+            None during training, but at test time these values should be passed in.
         feature_categories: Dict
             levels of categorical variables. None during training (they are inferred), but at
             test time these values should be passed in.
@@ -129,9 +129,9 @@ class DataProcessor:
 
         return df
 
-    def _transform_and_subset(self, df: pd.DataFrame) -> np.ndarray:
+    def _subset_and_binarize(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        Transform features and drop columns.
+        Drop unwanted columns and binarize selected features.
 
         Parameters
         ----------
@@ -140,8 +140,8 @@ class DataProcessor:
 
         Returns
         -------
-        np.ndarray
-            The array of features transformed and columns dropped.
+        pd.DataFrame
+            The data frame with removed and binarized columns.
         """
         # during the EDA, we decided to drop these
         keepers = list(set(VARIABLE_TYPE.keys()) - {"PAY", "ILP_R"})
@@ -164,55 +164,102 @@ class DataProcessor:
 
         df["M_Ht_In"] = df["M_Ht_In"].clip(48, 78)
 
+        # set the list of numeric and categorical features, to refer to in later methods
+        self.numeric_features = numeric_features
+        self.categorical_features = list(set(keepers) - set(numeric_features))
+
+        return df
+
+    def _prepare_numerical_features(self, df: pd.DataFrame) -> np.ndarray:
+        """
+        Prepare numerical features.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            The input data frame
+
+        Returns
+        -------
+        np.ndarray
+            The array of transformed numerical features.
+        """
+        # for regression and neural networks, unskew those four features
         if self.model_type in ["linear regression", "neural network"]:
             for feature in ["CIG_0", "PRIORDEAD", "PRIORTERM", "RF_CESARN"]:
                 df[feature] = np.log(df[feature] + 1)
 
-        x_numeric = df[numeric_features].values
+        x_numeric = df[self.numeric_features].values
+
+        # for the linear regression, we will decorrelate the continuous features
         if self.model_type == "linear regression":
             if self.standardization_parameters is None:
                 self.standardization_parameters = {}
-                self.standardization_parameters["means"] = np.nanmean(x_numeric, axis=0)
+                self.standardization_parameters["means"] = x_numeric.mean(axis=0)
                 q_matrix, r_matrix = np.linalg.qr(
                     x_numeric - self.standardization_parameters["means"]
                 )
                 self.standardization_parameters["r_matrix"] = r_matrix @ np.diag(
-                    np.nanstd(q_matrix, axis=0)
+                    q_matrix.std(axis=0) * 3.0
                 )
 
             x_numeric = (x_numeric - self.standardization_parameters["means"]) @ np.linalg.inv(
                 self.standardization_parameters["r_matrix"]
             )
 
+        # for the neural network, we will standardize without decorrelating
+        elif self.model_type == "neural network":
+            if self.standardization_parameters is None:
+                self.standardization_parameters = {}
+                self.standardization_parameters["means"] = np.nanmean(x_numeric, axis=0)
+                self.standardization_parameters["sds"] = np.nanstd(x_numeric, axis=0) * 3.0
+
+            x_numeric = (
+                x_numeric - self.standardization_parameters["means"]
+            ) / self.standardization_parameters["sds"]
+
         # tell the neural network where the missing data are
         if self.model_type == "neural network":
             x_numeric = np.hstack([x_numeric, np.where(np.isnan(x_numeric), 1, 0)])
 
-        categorical_features = list(set(keepers) - set(numeric_features))
+        return x_numeric
+
+    def _prepare_categorical_features(self, df: pd.DataFrame) -> np.ndarray:
+        """
+        Prepare categorical features.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            The input data frame
+
+        Returns
+        -------
+        np.ndarray
+            The array of one-hot encoded categorical features.
+        """
         if self.feature_categories is None:
             self.feature_categories = {}
-            for feature in categorical_features:
+            for feature in self.categorical_features:
                 self.feature_categories[feature] = np.sort(df[feature].dropna().unique())
 
         x_one_hot_list = []
-        for feature in categorical_features:
+        for feature in self.categorical_features:
             x_one_hot_col = OneHotEncoder(
                 categories=[self.feature_categories[feature]],
                 sparse_output=False,
-                drop=None,
+                drop=None if self.model_type == "linear regression" else "first",
                 handle_unknown="ignore",
             ).fit_transform(df[feature].values.reshape((-1, 1)))
 
             # put NAs back for the neural network
             if self.model_type == "neural network":
                 x_one_hot_col = x_one_hot_col * np.where(df[feature].isna(), np.nan, 1.0).reshape(
-                    -1, 1
+                    (-1, 1)
                 )
-
             x_one_hot_list.append(x_one_hot_col)
         x_one_hot = np.hstack(x_one_hot_list)
-
-        return np.hstack([x_one_hot, x_numeric])
+        return x_one_hot
 
     def __call__(self, df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
         """
@@ -226,10 +273,16 @@ class DataProcessor:
         Returns
         -------
         Tuple
-            the design matrix X and the response variable y.
+            the design matrix X and the response variable y, if present.
         """
         df = self._enforce_feature_types(df)
         df = self._process_missing_data(df)
-        X = self._transform_and_subset(df)
-        y = (df["DBWT"].values.reshape((-1, 1)) - Y_MEAN) / Y_SD
-        return X, y
+        df = self._subset_and_binarize(df)
+        X_numeric = self._prepare_numerical_features(df)
+        X_categorical = self._prepare_categorical_features(df)
+
+        if "DBWT" in df.columns:
+            y = (df["DBWT"].values.reshape((-1, 1)) - Y_MEAN) / Y_SD
+            return np.hstack([X_numeric, X_categorical]), y
+        else:
+            return np.hstack([X_numeric, X_categorical])
